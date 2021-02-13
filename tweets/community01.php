bae697580f20921fd9c70a6f5feb6acd1aefd2be
kdog0625 <kdoooog34@gmail.com>
2021-02-13 17:57:09 +0900

    投稿機能の準備開始

--- tweets/community01.php
+++ tweets/community01.php
@@ -2,6 +2,14 @@
   require('../common/header.php');
 ?> 
 
+<div class="main-top">
+  <div class="community-create">
+    <div class="tweet-top">
+      投稿する
+    </div>
+  </div>
+</div>
+
 <?php 
  require('../common/footer.php');
 ?>  
