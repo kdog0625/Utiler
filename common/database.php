669d609547d8c6016e65ce03e756fd6980c04b08
kdog0625 <kdoooog34@gmail.com>
2021-02-24 21:11:16 +0900

    リファクタリング

--- common/database.php
+++ common/database.php
@@ -7,6 +7,8 @@ function getDatabaseConnection() {
     try
     {
         $database_handler = new PDO('mysql:dbname=utiler;host = 127.0.0.1;port=8889;charset=utf8', 'root', 'root');
+        //PDO::ERRMODE_EXCEPTIONの値を設定することでエラーが発生したときに、PDOExceptionの例外を投げるように設定。
+        // $database_handler->setAttribute(setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING));
     }
     catch (PDOException $e)
     {
