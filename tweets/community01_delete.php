67be47e23de802dcbf19b0771a2d4eb8b7fa98b2
kdog0625 <kdoooog34@gmail.com>
2021-02-25 23:51:45 +0900

    削除機能実装

--- tweets/community01_delete.php
+++ tweets/community01_delete.php
@@ -7,12 +7,11 @@ require('../common/database.php');
 <?php
 require('../common/head_info.php');
 $database_handler = getDatabaseConnection();
-  // 削除するreviewのidの特定
-  $tweets=$database_handler->query('SELECT * FROM tweets WHERE id=?');
-  $tweets->execute();
-  $tweet= $tweets->fetch();
-    $tweet = $database_handler->prepare('DELETE FROM tweets WHERE id=?');
-    $tweet->execute(); 
+if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
+  $id=$_REQUEST['id'];
+  $tweets=$database_handler->prepare('DELETE FROM tweets WHERE id=?');
+  $tweets->execute(array($id));
+}
 print_r($tweets);
 header('Location:../tweets/community01.php'); //自分自身に遷移する
 exit();
