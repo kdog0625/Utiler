480650011e37aa5411af635101de553b10777b2c
kdog0625 <kdoooog34@gmail.com>
2021-02-26 00:18:58 +0900

    編集機能作成

--- tweets/community01_delete.php
+++ tweets/community01_delete.php
@@ -1,18 +1,18 @@
 <?php
 session_start();
+require('../common/auth.php');
 require('../common/validate.php');
-
 require('../common/database.php');
 ?>
 <?php
 require('../common/head_info.php');
+$user_id = getLoginUserId();
 $database_handler = getDatabaseConnection();
 if(isset($_REQUEST['id']) && is_numeric($_REQUEST['id'])){
   $id=$_REQUEST['id'];
   $tweets=$database_handler->prepare('DELETE FROM tweets WHERE id=?');
   $tweets->execute(array($id));
 }
-print_r($tweets);
 header('Location:../tweets/community01.php'); //自分自身に遷移する
 exit();
 ?>
--- tweets/community01_edit.php
+++ tweets/community01_edit.php
@@ -12,9 +12,14 @@ $database_handler = getDatabaseConnection();
 if(isset($_REQUEST['id'])&& is_numeric($_REQUEST['id'])){
   $id=$_REQUEST['id'];
 
-  $tweets=$database_handler->prepare('SELECT * FROM tweets WHERE id=:user_id');
-  $tweets->execute();
+  $tweets=$database_handler->prepare('SELECT * FROM tweets WHERE id=?');
+  $tweets->execute(array($id));
   $tweet=$tweets->fetch();
+}
+if(!empty($_POST)){
+  $statement=$database_handler->prepare('UPDATE tweets SET title=?,content=? WHERE id=?');
+  $statement->execute(array($_POST['title'],$_POST['content'],$_POST['id']));
+  header('Location: ../tweets/index.php');
 }
 ?>
 <?php 
@@ -24,11 +29,11 @@ require('../common/header.php');
 ?> 
 
 <div class="main-top">
-<form action="community01_update.php" method="post">
+<form action="" method="post">
   <!-- 以下の記述でデータを編集画面に渡す。 -->
   <input type="hidden" name="id" value="<?php print($id);?>">
-  <input type="text" name='title' placeholder="タイトル">
-  <textarea name="content" cols="50" rows="10"></textarea><br>
+  <input type="text" name='title' placeholder="タイトル" >
+  <textarea name="content" cols="50" rows="10"><?php print($tweet['content']);?></textarea><br>
   <button type="submit">登録する</button>
 </form>
 </div>
